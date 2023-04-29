cd7b563f5e2fae7c79b6a399ddff2e8146d0c6d6
Nikhil Arjun <nikhilarj338@gmail.com>
2023-04-29 20:00:54 +0530

    Added celsius conversion

--- Intoduction/Conversions.py
+++ Intoduction/Conversions.py
@@ -12,3 +12,18 @@ print(f'{kilometers} kilometers is equal to {miles} miles')
 
 
 # kilometers = miles / conv _fac
+
+# Python program to convert celsius to Fahrenheit
+
+# To convert a temperature in degree celsius and convert it into degree Fahrenheit.
+
+# fahrenheit = celsius * 1.8 + 32
+
+celsius = float(input("Enter Temperature in degrees celsius: "))
+
+# Calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print(f'{celsius} degrees is equal to {fahrenheit} degree fahrenheit.')
+
+# Conversion Fahrenheit to Celsius
+# celsius = (fahrenheit - 32) / 1.8
